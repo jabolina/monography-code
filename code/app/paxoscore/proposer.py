d8857d7dcbbeb43449bd1f28afbdee0dbbd03450
jabolina <jose.bolina@neppoadm.com.br>
2019-04-23 20:14:18 -0300

    defer only once

--- code/app/paxoscore/proposer.py
+++ code/app/paxoscore/proposer.py
@@ -55,9 +55,11 @@ class Proposer(DatagramProtocol):
             packed_size = struct.calcsize(fmt)
             unpacked_data = packer.unpack(datagram[:packed_size])
             req_id, result = unpacked_data
+
             logging.info("Response received [{}] with id [{}]".format(result, req_id))
-            self.defers[req_id].callback(result)
-            pass
+
+            if req_id in self.defers:
+                self.defers[req_id].callback(result)
+                self.defers.pop(req_id)
         except defer.AlreadyCalledError as ex:
-            logging.error("Error while handling response: [{}]".format(ex))
-            pass
+            logging.error("Error while handling response: [{}]".format(ex.message))
